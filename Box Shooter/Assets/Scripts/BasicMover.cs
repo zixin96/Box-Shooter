f973f9b087802ba527cd111039a0a00514aa6924
Zixin Zhang <zzhng151@illinois.edu>
2019-02-15 23:01:59 -0600

    First Commit

---
+++ Box Shooter/Assets/Scripts/BasicMover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour
+{
+    public float spinSpeed = 180.0f;
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
