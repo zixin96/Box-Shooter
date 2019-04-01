1e0d51be6551a784f1adf8b0fe02f031d2de236f
Zixin Zhang(Desktop) <zzhng151@illinois.edu>
2019-04-01 09:29:49 -0500

    Update 0.7

---
+++ Box Shooter/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    // DeathZone will restart the game upon collision
+    void OnCollisionEnter(Collision newCollision)
+    {
+        
+        if (newCollision.gameObject.tag == "Player")
+        {
+            // call the RestartGame function in the game manager
+            GameManager.gm.RestartGame();
+        }
+    }
+}
--- Box Shooter/Assets/Scripts/TargetMover.cs
+++ Box Shooter/Assets/Scripts/TargetMover.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class TargetMover : MonoBehaviour {
 
 	// define the possible states through an enumeration
-	public enum motionDirections {Spin, Horizontal, Vertical};
+	public enum motionDirections {Spin, Horizontal, Vertical, Circular, Diagonal};
 	
 	// store the state
 	public motionDirections motionState = motionDirections.Horizontal;
@@ -13,11 +13,20 @@ public class TargetMover : MonoBehaviour {
 	public float spinSpeed = 180.0f;
 	public float motionMagnitude = 0.1f;
 
-	// Update is called once per frame
-	void Update () {
+    // circular parameters: 5 seconds to complete a circle
+    public float timeToCompleteCircle = 5.0f;
+    public float radius = 5.0f;
+
+    private float angle = 0.0f;
+    private float circularSpeed = 0.0f;
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    // Update is called once per frame
+    void Update () {
 
 		// do the appropriate motion based on the motionState
-		switch(motionState) {
+		switch(motionState) { 
 			case motionDirections.Spin:
 				// rotate around the up axix of the gameObject
 				gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime);
@@ -32,6 +41,20 @@ public class TargetMover : MonoBehaviour {
                 // move up and down over time
                 gameObject.transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
                 break;
-		}
+
+
+            case motionDirections.Circular:
+                circularSpeed = (2.0f * Mathf.PI) / timeToCompleteCircle;
+                angle += circularSpeed * Time.deltaTime;
+                x = radius * Mathf.Cos(angle);
+                y = radius * Mathf.Sin(angle);
+                gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+                break;
+
+            case motionDirections.Diagonal:
+                gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+                gameObject.transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+                break;
+        }
 	}
 }
